acf005d31b4b1d6500da1f8f45c8c1ce370d1beb
nonoLin <denkanono@gmail.com>
2021-01-11 15:42:17 +0800

    rewrite seeder

--- app.js
+++ app.js
@@ -37,7 +37,6 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
   res.locals.warning_msg = req.flash('warning_msg')
-  // res.locals.error_msg = req.flash('error_msg')
   next()
 })
 
